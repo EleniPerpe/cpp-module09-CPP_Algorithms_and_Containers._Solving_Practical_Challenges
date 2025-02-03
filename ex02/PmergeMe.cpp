6031d788391b4847970cb671d6fb8df2711d64e9
Eleni <elperperidou@gmail.com>
2025-02-03 16:43:35 +0100

    logic for sortList ready

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: eleni <eleni@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 10:14:36 by eleni             #+#    #+#             */
-/*   Updated: 2025/02/03 11:43:45 by eleni            ###   ########.fr       */
+/*   Updated: 2025/02/03 16:26:32 by eleni            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,53 @@ PmergeMe::PmergeMe(int argc, char** argv)
             throw NoIntegerException();
         }        
     }
+    PmergeSortList(mergeList);
+    PmergeprintList(mergeList);
+    // PmergeprintList(mergeVector);
 }
 
-const char* PmergeMe::NoIntegerException::what() const throw()
+// Function to merge two sorted lists
+std::list<int> merge(std::list<int> left, std::list<int> right) 
 {
-    return "Exception: No integer input";
+    std::list<int> result;
+    
+    while (!left.empty() && !right.empty()) 
+    {
+        if (left.front() <= right.front()) 
+        {
+            result.push_back(left.front());
+            left.pop_front();
+        } else 
+        {
+            result.push_back(right.front());
+            right.pop_front();
+        }
+    }
+
+    // Add remaining elements
+    result.splice(result.end(), left);
+    result.splice(result.end(), right);
+    
+    return result;
+}
+
+// Merge Sort function for std::list<int>
+std::list<int> PmergeSortList(std::list<int> list) 
+{
+    std::cout << "Hello" << std::endl;
+
+    if (list.size() <= 1)
+        return list;
+    // Split the list into two halves
+    std::list<int> left, right;
+    auto mid = list.begin();
+    std::advance(mid, list.size() / 2);  // Move mid to the middle
+
+    left.splice(left.begin(), list, list.begin(), mid);
+    right.splice(right.begin(), list);
+
+    // Recursively sort both halves and merge them
+    return merge(PmergeSortList(left), PmergeSortList(right));
 }
 
 template <typename T>
@@ -49,4 +91,9 @@ void PmergeMe::PmergeprintList(const T& container)
     for (it = container.begin(); it != container.end(); it++)
         std::cout << *it << " ";
     std::cout << std::endl;
+}
+
+const char* PmergeMe::NoIntegerException::what() const throw()
+{
+    return "Exception: No integer (int) input";
 }
